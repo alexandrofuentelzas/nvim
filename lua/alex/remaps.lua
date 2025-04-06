1f9b4d4579d5468f87361edb14a89c6a08626c6c
Alexandro Fuentelzas <alex271@live.com>
2025-04-06 01:26:04 -0400

    initial neovim configs

---
+++ lua/alex/remaps.lua
@@ -0,0 +1,16 @@
+vim.g.mapleader = " "
+
+local function map(modes, lhs, rhs, opts)
+  opts = opts or {}
+  if type(modes) == "string" then
+    modes = { modes }
+  end
+  for _, mode in ipairs(modes) do
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+-- Remap hjkl to jkl; for normal and visual modes
+map({ "n", "v" }, "j", "h", { silent = true })
+map({ "n", "v" }, "k", "k", { silent = true })
+map({ "n", "v" }, ";", "j", { silent = true })
