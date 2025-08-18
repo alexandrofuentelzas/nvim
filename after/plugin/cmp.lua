3cdf0c0b79aa290ff0f4d1e758a1268a10e6e5b6
Alexandro Fuentelzas <alex271@live.com>
2025-08-18 01:07:15 -0400

    Hacky way to update visual color
    I updated the cmp plugin file which is used for
    text suggestions amongst other things. Since this
    plugin runs after init.lua, when I set the visual
    color it overrides the previous default configuration
    set by some other config. It works for now so I'll
    keep this here

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -42,3 +42,4 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- Make cmp popup background a little gray
 vim.api.nvim_set_hl(0, "Pmenu", { bg = "#2e2e2e" })
+vim.api.nvim_set_hl(0, "Visual", { bg = "#FFFF00" })
